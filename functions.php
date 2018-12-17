8fd4d030de532d28384e1b74ee148a1f680eaf60
elioschmutz <elio.schmutz@gmail.com>
2018-12-17 19:58:56 +0100

    Add shipping insurance

--- functions.php
+++ functions.php
@@ -24,3 +24,23 @@ if( !function_exists('mantis_child_theme_setup') ) {
         load_child_theme_textdomain( 'mantis-child', get_stylesheet_directory() . '/languages' );
     }
 }
+
+add_action('woocommerce_cart_totals_after_shipping', 'wc_shipping_insurance_note_after_cart');
+function wc_shipping_insurance_note_after_cart() {
+global $woocommerce;
+    $product_id = 661;
+foreach ( $woocommerce->cart->get_cart() as $cart_item_key => $values ) {
+    $_product = $values['data'];
+    if ( $_product->id == $product_id )
+        $found = true;
+    }
+    // if product not found, add it
+if ( ! $found ):
+?>
+    <tr class="shipping">
+        <th><?php _e( 'Shipping Insurance', 'mantis-child' ); ?></th>
+        <td><a href="<?php echo do_shortcode('[add_to_cart_url id="661"]'); ?>"><?php _e( 'Add shipping insurance', 'mantis-child' ); ?> </a></td>
+    </tr>
+<?php endif;
+}
+
